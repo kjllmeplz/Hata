bc6f0f92538b88ec1da6103b06b04d9104917027
Ranko <kjllmeplz@gmail.com>
2023-12-10 18:56:59 +0700

    Add Interconnectivity

---
+++ app/src/main/java/com/ranko/hata/ModuleConst.kt
@@ -0,0 +1,8 @@
+package com.ranko.hata
+
+object ModuleConst {
+    const val SettingsPackage = "com.android.settings"
+    val MenuRemoveLists = listOf("security_status", "safety_emergency_settings", "feedback_services_settings")
+    val MenuMoveLists = listOf("interconnection_settings")
+    val GGTitle: CharSequence = "Google"
+}
--- app/src/main/java/com/ranko/hata/ModuleHook.kt
+++ app/src/main/java/com/ranko/hata/ModuleHook.kt
@@ -17,39 +17,63 @@ class ModuleHook {
             fun after(param: XposedInterface.AfterHookCallback) {
                 val mContext = (param.thisObject as Activity).baseContext
 
-                val ids = ArrayList<Int>()
+                //Build remove menu lists
+                val removeIds = ArrayList<Int>()
                 for (id in ModuleConst.MenuRemoveLists) {
-                    ids.add(mContext.resources.getIdentifier(id, "id", mContext.packageName))
+                    removeIds.add(mContext.resources.getIdentifier(id, "id", mContext.packageName))
+                }
+
+                //Build move menu lists
+                val moveIds = ArrayList<Int>()
+                for (id in ModuleConst.MenuMoveLists) {
+                    moveIds.add(mContext.resources.getIdentifier(id, "id", mContext.packageName))
                 }
 
                 val headers = param.args[0] as MutableList<Any>
                 val removeIDs = ArrayList<Int>()
                 var ggHeader: Any? = null
+                val moveMenus = ArrayList<Any?>()
 
                 for ((i, head) in headers.withIndex()) {
+                    //Get list ID remove
                     val fieldID = head.javaClass.getDeclaredField("id")
-                    for (id in ids) {
+                    for (id in removeIds) {
                         if (fieldID.getLong(head) == id.toLong()) {
                             removeIDs.add(i)
                             break
                         }
                     }
 
+                    //Get list ID move
+                    for (id in moveIds) {
+                        if (fieldID.getLong(head) == id.toLong()) {
+                            removeIDs.add(i)
+                            moveMenus.add(head)
+                            break
+                        }
+                    }
+
+                    //Get Google
                     val fieldTitle = head.javaClass.getDeclaredField("title")
                     if (fieldTitle.get(head) != null) {
-                        if (fieldTitle.get(head)?.equals(ModuleConst.ggTitle) == true) {
+                        if (fieldTitle.get(head)?.equals(ModuleConst.GGTitle) == true) {
                             removeIDs.add(i)
                             ggHeader = head
                         }
                     }
                 }
 
+                //Remove menu
                 if (removeIDs.isNotEmpty()) {
                     removeIDs.sortDescending()
                     for (removeID in removeIDs) headers.removeAt(removeID)
                 }
 
+                //Re-add Google
                 if (ggHeader != null) headers.add(ggHeader)
+
+                //Re-add others
+                for (menuID in moveMenus) headers.add(menuID!!)
 
                 module.log("Rearrange menu success!")
             }
